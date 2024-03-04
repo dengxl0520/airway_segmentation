ae6d8d99969c5bff6a4f9446087e643290dd482d
DengXL <dengxl0520@gmail.com>
2024-03-04 14:33:41 +0800

    [add] train script

--- scripts/baseline_ATM.sh
+++ scripts/baseline_ATM.sh
@@ -1,6 +1,6 @@
-CUDA_VISIBLE_DEVICES=3 python main.py --model baseline_fr_ds \
+CUDA_VISIBLE_DEVICES=2 python main.py --model baseline \
 --dataset ATM -b 2 --workers 8 \
---save-dir baseline_fr_ds_ATM \
+--save-dir baseline_ATM \
 --sadencoder 0 --saddecoder 0 \
 --cubesize 80 192 304 --stridet 64 96 152 --stridev 64 72 72 \
 --start-epoch 1 --epoch 60 --sgd 0 \
